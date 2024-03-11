47f0e11527cb601973eaf1d402f45944e510d6e4
RudenkoAD <rudenko.ad@phystech.edu>
2024-03-11 18:55:34 +0300

    add theme cubit

---
+++ lib/domain/bloc/theme_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeCubit extends Cubit<ThemeData> {
+  ThemeCubit() : super(ThemeCubit.darkTheme);
+
+  static final darkTheme = ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 7, 5, 43)),
+            textTheme: GoogleFonts.rajdhaniTextTheme(),
+        );
+  static final lightTheme = ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 230, 204, 133)),
+            textTheme: GoogleFonts.rajdhaniTextTheme(),
+        );
+  
+  void changeTheme() {
+    emit(state == darkTheme ? lightTheme : darkTheme);
+  }
+}
