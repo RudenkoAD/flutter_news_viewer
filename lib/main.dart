4a9fa59c01c615e014e9e550783c8c135189adfd
RudenkoAD <rudenko.ad@phystech.edu>
2024-03-31 02:21:00 +0300

    remove unused import from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'internal/application.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter_news_viewer/domain/bloc/provider.dart' as provider;
-
-final pageProvider = provider.pageProvider;
-final favouriteProvider = provider.favouriteProvider;
-final themeProvider = provider.themeProvider;
 
 Future main() async {
   runApp(
