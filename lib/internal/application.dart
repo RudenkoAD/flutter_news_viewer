b9af14fe6515f5a0fac6f8983533adcc0116ca4e
RudenkoAD <rudenko.ad@phystech.edu>
2024-03-31 03:09:48 +0300

    update app to use riverpod

--- lib/internal/application.dart
+++ lib/internal/application.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_news_viewer/domain/bloc/page_bloc.dart';
 import 'package:flutter_news_viewer/frontend/article_page.dart';
 import 'package:flutter_news_viewer/frontend/home_page.dart';
 import 'package:flutter_news_viewer/frontend/news_page.dart';
-import 'package:flutter_news_viewer/domain/bloc/theme_cubit.dart';
-
-class App extends StatelessWidget {
+import 'package:flutter_news_viewer/frontend/favourite_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_news_viewer/domain/bloc/provider.dart';
+class App extends ConsumerWidget {
   const App({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => PageBloc()),
-        BlocProvider(create: (context) => ThemeCubit()),
-      ],
-      child: const AppWindow(),
-    );
-  }
-}
-
-class AppWindow extends StatelessWidget {
-  const AppWindow({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<ThemeCubit, ThemeData>(builder: (context, theme) {
+  Widget build(BuildContext context, WidgetRef ref) {
       return MaterialApp(
           title: 'Flutter News Viewer',
-          theme: theme,
+          theme: ref.watch(themeProvider),
           home: const HomePage(),
           routes: {
             '/news': (context) => const NewsPage(),
             '/article': (context) => const ArticlePage(),
             '/home': (context) => const HomePage(),
+            '/favourite': (context) => const FavouritePage(),
           });
-    });
+    }
   }
-}
