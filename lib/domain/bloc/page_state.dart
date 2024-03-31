c000bcb7afccb8fd113eb6e521c0df96a92877e3
RudenkoAD <rudenko.ad@phystech.edu>
2024-03-31 21:15:12 +0300

    change starting page to 1

--- lib/domain/bloc/page_state.dart
+++ lib/domain/bloc/page_state.dart
@@ -77,7 +77,7 @@ final class PageState extends Equatable {
       this.category,
       this.q,
       this.pageSize = 20,
-      this.page = 0});
+      this.page = 1});
 
   PageState copyWith({
     String? apiKey,
